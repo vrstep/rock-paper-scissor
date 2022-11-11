14fdf1984a1c39f124cae0be4a8405836b5ceded
Amir <amir.batyrggaliev@gmail.com>
2022-11-11 12:23:49 +0600

    Add playerSelection logic

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+let computerChoice = ['rock', 'paper', 'scissors'];
+
+let playerSelection = prompt("Enter your choice").toLowerCase();
+console.log(playerSelection);
+let computerSelection = getComputerChoice(computerChoice);
+
 function getComputerChoice(computerChoice) {
     let rand = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[rand];
 }
 
-let computerChoice = ['rock', 'paper', 'scissors'];
+function playRound(playerSelection, computerSelection) {
 
-let playerSelection;
-let computerSelection;
-
-console.log(getComputerChoice(computerChoice));
+}
