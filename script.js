b44be7f76a3ea6b285cf5c1eac87d72a9aafc33a
Amir <amir.batyrggaliev@gmail.com>
2022-11-11 12:08:05 +0600

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function getComputerChoice() {
-    
+let computerChoice = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice(computerChoice) {
+    let rand = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[rand];
 }
+
+console.log(getComputerChoice(computerChoice));
