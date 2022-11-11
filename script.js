331f831bf9fc333734024a9b710a3783578739e3
Amir <amir.batyrggaliev@gmail.com>
2022-11-11 12:13:56 +0600

    Add playRound function

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ function getComputerChoice(computerChoice) {
 
 let computerChoice = ['rock', 'paper', 'scissors'];
 
+let playerSelection;
+let computerSelection;
+
 console.log(getComputerChoice(computerChoice));
