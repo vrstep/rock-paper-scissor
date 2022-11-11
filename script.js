7171bf5245da9ee5bbac5d6e755a9a191e542329
Amir <amir.batyrggaliev@gmail.com>
2022-11-11 12:11:13 +0600

    Add playerSelection and computerSelection varialbes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let computerChoice = ['rock', 'paper', 'scissors'];
-
 function getComputerChoice(computerChoice) {
     let rand = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[rand];
 }
+
+let computerChoice = ['rock', 'paper', 'scissors'];
 
 console.log(getComputerChoice(computerChoice));
