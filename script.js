339dc1d6ef19a769449e47c337619ee8ea5b36c4
Amir <amir.batyrggaliev@gmail.com>
2022-11-11 14:16:30 +0600

    Redo the playRound function

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-let computerChoice = ['rock', 'paper', 'scissors'];
+let computerChoice = ['rock', 'paper', 'scissor'];
 
-let playerSelection = prompt("Enter your choice").toLowerCase();
-let computerSelection = getComputerChoice(computerChoice);
+let playerSelection;
+let computerSelection;
 
 function getComputerChoice(computerChoice) {
     let rand = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[rand];
 }
 
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection > computerSelection) {
+//         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+//     } else console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+// }
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection > computerSelection) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    } else console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    // In case player is winning
+    if (playerSelection == 'rock' && computerSelection == 'scissor') {
+        console.log('You win! Rock beats scissor');
+    } 
+    else if (playerSelection == 'scissor' && computerSelection == 'paper') {
+        console.log('You win! Scissor beats paper');
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log('You win! Paper beats rock');
+    }
+    // In case computer is winning
+    else if (computerSelection == 'rock' && playerSelection == 'scissor') {
+        console.log('You lose! Rock beats scissor');
+    }
+    else if (computerSelection == 'scissor' && playerSelection == 'paper') {
+        console.log('You lose! Scissor beats paper');
+    }
+    else if (computerSelection == 'paper' && playerSelection == 'rock') {
+        console.log('You lose! Paper beats rock');
+    }
+    else console.log("It's a tie!");
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Enter your choice").toLowerCase();
+        console.log(playerSelection);
+        computerSelection = getComputerChoice(computerChoice);
+        console.log(computerSelection);
+        console.log(`Round ${i}`);
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+console.log(game())
